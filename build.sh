65698856e8ecfdb5345aed889fee2cf149e6809b
Iku Iwasa <iku.iwasa@gmail.com>
2018-12-24 13:59:56 +0900

    Workaround for Vagrant coreos guest network configuration

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ if [ ! -f id_rsa ]; then
 	curl -o id_rsa https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant
 fi
 
-ct -pretty -in-file ignition.yml -out-file ignition.json
+ct -pretty -in-file ignition.yml | fx 'x => {
+   x.systemd.units[0].dropins[0].contents += "\n[Install]\nAlias = etcd.service";
+   return x
+}' > ignition.json
 
 release=alpha
 case $1 in
